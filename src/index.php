7121b1df11e113e2960899a40edadee4cda4ff35
mateus <mateus@mateus-VPCCW23FX>
2015-02-05 12:08:11 -0200

    Commit estrutura - V 1.0

--- src/index.php
+++ src/index.php
@@ -4,6 +4,14 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+
+
+error_reporting(E_ALL ^ E_NOTICE);
+include_once './app/libs/php/firephp/fb.php';
+ob_start();
+
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +19,11 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo 'HELLO!!!!!';
+        try{
+            fb('HELLO!!!!!'); 
+        } catch (Exception $ex) {
+            echo $ex->getMessage();
+        }
         ?>
     </body>
 </html>
